0c1adf418654d5a12a031e231c567765bee3f222
Mayank-Joshi01 <mayankjoshi.in.123@gmail.com>
2026-02-18 01:16:18 +0530

    Created basic structure for Server and Client and Connected Docker with it

---
+++ Client/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    
+    <div className="text-2xl font-bold text-blue-600">Welcome to the MailMe Client App! it is running through Docker Compose.</div>
+    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Click Me</button>
+    <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Sign in</button>
+    <button className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded">Sign Up</button>
+    </>
+  )
+}
+
+export default App
